ea3ed9a9e55702bbe809842b83e1da37fc434609
Andrey Perepechaev <perepechaev@inbox.ru>
2012-10-06 18:44:18 +0400

    Reload key-binding file

--- etc/key-binding.php
+++ etc/key-binding.php
@@ -1,11 +1,36 @@
 <?php
 
-$commands = array(
-    'q й' => 'command_exit',
+return array(
+    'q й' => 'command_scrolloff',
     'i ш' => 'command_prompt',
-    'd в' => 'command_debug',
+    '` ё' => 'command_debug',
+
+    'Q Й' => 'command_exit',
+
     KEY_PAGEUP 		=> 'command_scrollup',
     KEY_PAGEDOWN  	=> 'command_scrolldown',
     KEY_UP          => 'command_scrollup_one',
     KEY_DOWN        => 'command_scrolldown_one',
+
+    'j о'   => 'command_send|юг',
+    'h р'   => 'command_send|запад',
+    'l д'   => 'command_send|восток',
+    'k л'   => 'command_send|север',
+    'u г'   => 'command_send|вверх',
+    'd в'   => 'command_send|вниз',
+
+    "' э"   => 'command_send|эффекты',
+    "v м"   => 'command_send|инвентарь',
+    "b и"   => 'command_send|экипировка',
+    "c с"   => 'command_send|счёт',
+
+    'w ц'   => 'command_send|взять коврик старый; нелетать; брос коврик; отд коврик; укутаться',
+    'e у'   => 'command_send|встать; взять коврик; полож коврик старый; гов торнел летать не хочешь?',
+    'W Ц'   => 'command_send|спать',
+    'E У'   => 'command_send|отдых',
+
+    'x ч'   => 'command_send|кто',
+    'z я'   => 'command_send|колдовать родник; пить; пить; отрыж',
+
+    KEY_ESC => 'command_send|встать; вз коврик; колд "слово возврата"',
 );
--- lib/client/commands.php
+++ lib/client/commands.php
@@ -4,26 +4,84 @@ $GLOBALS['history']['cursor'] = false;
 
 function command_manager($command){
     global $commands;
-    static $cache;
-    if ( !$cache ){
-        foreach ( $commands as $key => $function ){
-            $key = iconv( 'UTF-8', 'KOI8-R', $key );
-            foreach ( explode(' ', $key ) as $k ){
-                $cache[$k] = $function;
-            }
-        }
-    }
 
     write_log(sprintf( "KEY: 0x%x 0d%d %s", $command, $command, chr($command) ));
 
-	if ( isset( $cache[$command]) ){
-		return $cache[$command]();
-	}
+    $cache = load_key_binding();
 
-    if ( isset( $cache[chr($command)] ) ){
-        return $cache[chr($command)]();
+    $func = false;
+    if ( isset( $cache[$command]) ){
+        $func = $cache[$command];
     }
 
+    if ( isset( $cache[chr($command)] ) ){
+        $func = $cache[chr($command)];
+    }
+
+    if ($func){
+        return call_user_func($func[0], $func[1]);
+    }
+
+    return false;
+}
+
+function load_key_binding(){
+    static $cache, $loaded;
+    $old = $cache;
+
+    clearstatcache();
+
+    $filename = __DIR__ . '/../../etc/key-binding.php';
+    write_log('Check: ' . filemtime($filename) . ': ' . date('Y-m-d H:i:s', filemtime($filename)));
+
+    if ($loaded && filemtime($filename) !== $loaded){
+        $cache = null;
+    };
+
+    if ($cache){
+        return $cache;
+    }
+
+    $loaded = filemtime($filename);
+    if (php_check_syntax($filename, $error)){
+        $commands = include $filename;
+        write_log('Loaded key binding');
+    }
+    else {
+        $commands = array();
+        $cache = $old;
+
+        write_log('Syntax error: ' . implode("\n", $error));
+    }
+
+    foreach ( $commands as $key => $command){
+        $key = iconv( 'UTF-8', 'KOI8-R', $key );
+        $command = iconv('UTF-8', 'KOI8-R', $command);
+
+        $function = array();
+        $pos = strpos($command, '|');
+        if ($pos){
+            $function[] = substr($command, 0, $pos);
+            $function[] = explode(";", substr($command, $pos + 1));
+        }
+        else {
+            $function[] = $command;
+            $function[] = array();
+        }
+
+        foreach ( explode(' ', $key ) as $k ){
+            $cache[$k] = $function;
+        }
+    }
+
+    return $cache;
+}
+
+function php_check_syntax($file, &$error) {
+    exec("php -l $file", $error, $code);
+    if( $code == 0 ){
+        return true;
+    }
     return false;
 }
 
@@ -118,4 +176,18 @@ function command_scrollup_one(){
 function command_scrolldown_one(){
 	global $iostream;
     $iostream->get('output')->scroll(+1);
+}
+
+function command_scrolloff(){
+    global $iostream;
+    $iostream->get('output')->scroll(+10000);
+}
+
+function command_send($arg){
+    global $iostream, $socket;
+
+    $iostream->get('output')->addstr("\033[0;34m" . implode(";", $arg) . "\033[0m");
+    foreach ($arg as $cmd){
+        $status = @socket_write( $socket, $cmd . "\n" );
+    }
 }
